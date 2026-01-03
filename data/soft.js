907b2b6173ba8e92849a71ab2e1aebc8122daf86
rady112 <rady_h_@hotmail.com>
2026-01-03 04:43:43 +0200

    Update soft.js

--- data/soft.js
+++ data/soft.js
@@ -1,1 +1,7 @@
-
+export default [
+  { id: "sf1", name: "Cola" },
+  { id: "sf2", name: "Sprite" },
+  { id: "sf3", name: "Cola - Zero" },
+  { id: "sf4", name: "Sprite - Zero" },
+  { id: "sf5", name: "Water" }
+];
