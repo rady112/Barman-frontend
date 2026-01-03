f4ad21cc5a4ea1c3022b31b1e92263c485dbc2c9
rady112 <rady_h_@hotmail.com>
2026-01-03 04:41:26 +0200

    Update specials.js

--- data/specials.js
+++ data/specials.js
@@ -1,1 +1,5 @@
-
+export default [
+  { id: "rs1", name: "Love Potion", ingredients: [] },
+  { id: "rs2", name: "Spicy Halaweh", ingredients: [] },
+  { id: "rs3", name: "The Iceberg", ingredients: [] },
+];
